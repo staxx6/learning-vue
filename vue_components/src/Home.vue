09426815cadcc7f559cebcd190a47df3a9687192
Simon Oczadly <staxx@hotmail.de>
2019-04-19 00:02:56 +0200

    update

---
+++ vue_components/src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <app-serverr-status v-for="server in 5"></app-serverr-status>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './ServerStatus';
+
+    export default {
+        components: {
+            'app-serverr-status': ServerStatus
+        }
+    }
+</script>
+
